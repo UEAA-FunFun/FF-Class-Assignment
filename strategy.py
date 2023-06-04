7e1bd312bdba268de76772255856113c64893e20
justinzhang <justinz@andrew.cmu.edu>
2023-06-04 12:26:10 -0400

    summary stats updated to give more info

--- strategy.py
+++ strategy.py
@@ -30,22 +30,48 @@ class Strategy:
 
             print(f"{cname}: {csize} out of {ccap}")
         
-        aMiss = 0
-        pMiss = 0
-        bothMiss = 0
-        for rid in range(self.rutil.getSize()):
-            if not self.rutil.isRegistered(rid,"AM") and not self.rutil.isRegistered(rid,"PM"):
-                bothMiss += 1
-            elif not self.rutil.isRegistered(rid,"AM"):
-                aMiss += 1
+        #keep a log of the students who do not have a class
+        aMiss,aLog = 0, []
+        pMiss,pLog = 0, []
+        bothMiss,bLog = 0, []
 
-            elif not self.rutil.isRegistered(rid,"PM"):
+        for rid in range(self.rutil.getSize()):
+            #check that the student is neither registered for AM nor PM, and that their list of choices is not empty
+            if not self.rutil.isRegistered(rid,"AM") and not self.rutil.isRegistered(rid,"PM") and self.rutil.getTimeChoices(rid,"AM",self.cutil) != [] and self.rutil.getTimeChoices(rid,"PM",self.cutil) != []:
+                bothMiss += 1
+                bLog.append(rid)
+            elif not self.rutil.isRegistered(rid,"AM") and self.rutil.getTimeChoices(rid,"AM",self.cutil) != []:
+                aMiss += 1
+                aLog.append(rid)
+
+            elif not self.rutil.isRegistered(rid,"PM") and self.rutil.getTimeChoices(rid,"PM",self.cutil) != []:
                 pMiss += 1
+                pLog.append(rid)
 
         print("\n--------------------------------Missing Classes--------------------------------")
         print(f"{aMiss} students do not have an AM class")
+        #if a student without an AM class exists, print their class choices
+        if aMiss > 0:
+            print("Their choices are:")
+            for rid in aLog:
+                print(f"Student {rid}: {self.rutil.getTimeChoices(rid,'AM',self.cutil)}")
         print(f"{pMiss} students do not have an PM class")
+        #if a student without an PM class exists, print their class choices
+        if pMiss > 0:
+            print("Their choices are:")
+            for rid in pLog:
+                print(f"Student {rid}: {self.rutil.getTimeChoices(rid,'PM',self.cutil)}")
         print(f"{bothMiss} have neither\n")
+        if bothMiss > 0:
+            print("Their choices are:")
+            for rid in bLog:
+                print(f"Student {rid}: {self.rutil.getTimeChoices(rid,'AM',self.cutil)} and {self.rutil.getTimeChoices(rid,'PM',self.cutil)}")
+
+        self.bLog = bLog
+        self.aLog = aLog
+        self.pLog = pLog
+    
+    
 
 
 
@@ -58,7 +84,7 @@ class GreedyRandomStudent(Strategy):
     def eval(self):
         for rid in self.lotto:
             amChoices = self.rutil.getTimeChoices(rid,"AM",self.cutil)
-            pmChoices = self.rutil.getTimeChoices(rid,"PM",self.cutil)
+            #pmChoices = self.rutil.getTimeChoices(rid,"PM",self.cutil)
 
 
             for currClass in amChoices:
@@ -66,8 +92,10 @@ class GreedyRandomStudent(Strategy):
                 if Utils.addToClass(cid,rid,self.cutil,self.rutil):
                     break
 
+        self.lotto = self.shuffle()
+        for rid in self.lotto:
+            pmChoices = self.rutil.getTimeChoices(rid,"PM",self.cutil)
             for currClass in pmChoices:
                 cid = self.cutil.getId(currClass)
                 if Utils.addToClass(cid,rid,self.cutil,self.rutil):
-                    break
-
+                    break 
